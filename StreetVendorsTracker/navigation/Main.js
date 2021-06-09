2ed69cb3416b3e60aed1cba709d736414e00e652
pierre ANTOINE <pierre.ant7@hotmail.com>
2021-06-09 05:12:44 +0200

    npm install @react-native-firebase/firestore

--- StreetVendorsTracker/navigation/Main.js
+++ StreetVendorsTracker/navigation/Main.js
@@ -1,13 +1,20 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginPage from '../page/Login';
 
+import { useAuth } from '../contexts/Auth';
+
+
 const Stack = createStackNavigator();
 
+
+
 export default function Main() {
+
+    const { user } = useAuth();
+
     return (
             <Stack.Navigator>
                 <Stack.Screen name="Login" component={LoginPage} />
