38d364f4032741fe94311a3969c8506a7c0e0839
pierre ANTOINE <pierre.ant7@hotmail.com>
2021-06-09 10:06:18 +0200

    can display map

--- StreetVendorsTracker/page/Discover.js
+++ StreetVendorsTracker/page/Discover.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
+import MapView from "react-native-maps";
+
 export default function Discover() {
     return (
         <View>
             <Text>Discover page</Text>
+            <MapView
+                style={{ height: 100, width: 100 }}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                }}
+            />
         </View>
     )
 }
