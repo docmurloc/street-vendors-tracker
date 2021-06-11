c997b3f11e68fcac16679993211bdc6cb7f4e7a9
pierre ANTOINE <pierre.ant7@hotmail.com>
2021-06-12 00:31:43 +0200

    can retrive timetable from firestore

--- StreetVendorsTracker/components/TimeSliders.js
+++ StreetVendorsTracker/components/TimeSliders.js
@@ -4,9 +4,33 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
 
+import { useAuth } from '../contexts/Auth';
+
+import firestore from '@react-native-firebase/firestore';
+
 export default function TimeSlider({ day, handleValue }) {
 
     const [selected, setSelected] = useState([0, 24]);
+
+    const { user } = useAuth();
+
+    useEffect(async () => {
+        if (user) {
+            const timeTable = await firestore()
+                .collection('TimeTable')
+                .doc(user.providerData[0].uid)
+                .get();
+
+            console.log('timeTable data: ', timeTable.data());
+
+            let data = [0, 23]
+
+            data[0] = timeTable.data()[day]?.from ? timeTable.data()[day].from : 0;
+            data[1] = timeTable.data()[day]?.to ? timeTable.data()[day].to : 24;
+            setSelected(data)
+
+        }
+    }, [user])
 
     useEffect(() => {
         console.log(`${day} from ${selected[0]} to ${selected[1]}`)
