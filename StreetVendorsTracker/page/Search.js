65f315916d5aa02d09146a8d269342a39e731529
pierre ANTOINE <pierre.ant7@hotmail.com>
2021-06-12 05:52:38 +0200

    create search header component

--- StreetVendorsTracker/page/Search.js
+++ StreetVendorsTracker/page/Search.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
+import SearchHeader from '../components/SearchHeader';
+
 export default function Search() {
     return (
         <View>
             <Text>Search page</Text>
+            <SearchHeader/>
         </View>
     )
 }
