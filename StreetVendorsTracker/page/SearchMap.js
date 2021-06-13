d1dcfd2953569135229620e0bdd175791ff2b0b2
pierre ANTOINE <pierre.ant7@hotmail.com>
2021-06-13 07:04:27 +0200

    npm i geolib

--- StreetVendorsTracker/page/SearchMap.js
+++ StreetVendorsTracker/page/SearchMap.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+import MapView, { Marker } from "react-native-maps";
+
+import { useSearch } from '../contexts/Search';
+import { usePosition } from '../contexts/Position';
 
 import { View, Text } from 'react-native';
 
 import SearchHeader from '../components/SearchHeader';
 
 export default function SearchMap() {
+
+    const { searchPosition } = useSearch();
+
+
+    const positionMarker = {
+        ...searchPosition
+    }
+
     return (
         <View>
             <Text>Search map page</Text>
